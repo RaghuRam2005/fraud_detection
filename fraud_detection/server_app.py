c522dd0b27d478ae71e646d022e788cb12642c95
RaghuRam2005 <atommail20@gmail.com>
2025-10-16 19:18:30 +0530

    update server_app to display loss graph

--- fraud_detection/server_app.py
+++ fraud_detection/server_app.py
@@ -1,13 +1,42 @@
 """fraud-detection: A Flower / PyTorch app."""
 
 import torch
-from flwr.app import ArrayRecord, ConfigRecord, Context
+from typing import Iterable, Optional
+from flwr.app import ArrayRecord, ConfigRecord, Context, Message, MetricRecord
 from flwr.serverapp import Grid, ServerApp
 from flwr.serverapp.strategy import FedAvg
+import matplotlib.pyplot as plt
 
 from fraud_detection.task import Net
 
 app = ServerApp()
+
+def plot_loss_curve(loss_list):
+    plt.figure(figsize=(8, 5))
+    plt.plot(range(1, len(loss_list) + 1), loss_list, marker='o', linestyle='-', linewidth=2)
+    plt.title("Training Loss per Epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.grid(True)
+    plt.show()
+
+
+class CustomFedAvg(FedAvg):
+  def aggregate_train(
+        self,
+        server_round: int,
+        replies: Iterable[Message],
+    ) -> tuple[Optional[ArrayRecord], Optional[MetricRecord]]:
+        """Aggregate ArrayRecords and MetricRecords in the received Messages."""
+
+        for reply in replies:
+            if reply.has_content():
+                # Retrieve the ConfigRecord from the message
+                train_losses = reply.content["loss-history"]
+                plot_loss_curve(train_losses)
+                
+        # Aggregate the ArrayRecords and MetricRecords as usual
+        return super().aggregate_train(server_round, replies)
 
 @app.main()
 def main(grid:Grid, context:Context):
