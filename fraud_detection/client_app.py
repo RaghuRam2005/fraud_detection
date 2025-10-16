f54bf71c00be82df8fbc0ae198df98d50ad36c48
RaghuRam2005 <atommail20@gmail.com>
2025-10-16 19:44:33 +0530

    update wandb logging

--- fraud_detection/client_app.py
+++ fraud_detection/client_app.py
@@ -12,56 +12,56 @@ app = ClientApp()
 
 @app.train()
 def train(msg:Message, context:Context):
-  model = Net()
-  model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
-  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  model.to(device)
+    model = Net()
+    model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-  partition_id = context.node_config["partition-id"]
-  num_partitions = context.node_config["num-partitions"]
-  trainloader, _ = load_data(partition_id, num_partitions)
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    trainloader, _ = load_data(partition_id, num_partitions)
 
-  train_loss = train_fn(
-      model,
-      trainloader,
-      epochs=context.run_config["local-epochs"],
-      lr=msg.content["config"]["lr"],
-      device=device,
-  )
+    train_loss = train_fn(
+        model,
+        trainloader,
+        epochs=context.run_config["local-epochs"],
+        lr=msg.content["config"]["lr"],
+        device=device,
+    )
 
-  model_record = ArrayRecord(model.state_dict())
-  metrics = {
-      "train-loss":train_loss[-1],
-      "loss-history":train_loss,
-      "num-examples":len(trainloader.dataset),
-  }
-  metrics_record = MetricRecord(metrics)
-  content = RecordDict({"arrays":model_record, "metrics":metrics_record})
-  return Message(content=content, reply_to=msg)
+    model_record = ArrayRecord(model.state_dict())
+    metrics = {
+        "train-loss":train_loss[-1],
+        "loss-history":train_loss,
+        "num-examples":len(trainloader.dataset),
+    }
+    metrics_record = MetricRecord(metrics)
+    content = RecordDict({"arrays":model_record, "metrics":metrics_record})
+    return Message(content=content, reply_to=msg)
 
 @app.evaluate()
 def evaluate(msg:Message, context:Context):
-  model = Net()
-  model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
-  device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  model.to(device)
+    model = Net()
+    model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-  partition_id = context.node_config["partition-id"]
-  num_partitions = context.node_config["num-partitions"]
-  _, valloader = load_data(partition_id, num_partitions)
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    _, valloader = load_data(partition_id, num_partitions)
 
-  eval_loss, eval_acc = test_fn(
-      model,
-      valloader,
-      device=device,
-  )
+    eval_loss, eval_acc = test_fn(
+        model,
+        valloader,
+        device=device,
+    )
 
-  metrics = {
-      "eval-loss" : eval_loss,
-      "eval-acc" : eval_acc,
-      "num-examples" : len(valloader.dataset),
-  }
+    metrics = {
+        "eval-loss" : eval_loss,
+        "eval-acc" : eval_acc,
+        "num-examples" : len(valloader.dataset),
+    }
 
-  metric_record = MetricRecord(metrics)
-  content = RecordDict({"metrics":metric_record})
-  return Message(content=content, reply_to=msg)
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({"metrics":metric_record})
+    return Message(content=content, reply_to=msg)
